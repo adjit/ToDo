6708f3fc5e3809fa5852c6e37566c5b4a8c2f9e2
adjit <ajtawil@gmail.com>
2019-09-09 23:57:47 -0400

    Add project files.

---
+++ ToDo/Models/ToDoList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDo.Models
+{
+    public class ToDoList
+    {
+        public ToDoList()
+        {
+            ToDoListItems = new HashSet<ToDoListItem>();
+        }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string ToDoListName { get; set; }
+        public string ToDoListDescription { get; set; }
+
+        public ICollection<ToDoListItem> ToDoListItems { get; set; }
+    }
+}
---
+++ ToDo/Models/ToDoListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDo.Models
+{
+    public class ToDoListItem
+    {
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string ToDoItem { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+
+        //Foreign Key
+        public long ToDoListId { get; set; }
+
+    }
+}
